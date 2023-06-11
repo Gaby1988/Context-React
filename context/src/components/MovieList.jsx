dba67ab7ac508ee7a099fef773559c3df1364602
Gaby1988 <110187007+Gaby1988@users.noreply.github.com>
2023-06-11 11:14:43 +0200

    Update MovieList.jsx

--- context/src/components/MovieList.jsx
+++ context/src/components/MovieList.jsx
@@ -1,36 +1,37 @@
+
 import React, { useContext } from "react";
 import { MovieContext } from "../contexts/MovieContext";
 
-
 function MovieList() {
-	const { movies, setMovies } = useContext(MovieContext);
-	return (
-		<>
-			<h1>MoviesList</h1>
-			<ul>
-				{movies.map((movie) => (
-					<li key={movie.movie_id}>
-						{movie.title} ({movie.release_year})
-					</li>
-				))}
-			</ul>
-			{movies.length < 4 && (
-				<button
-					onClick={() => {
-						setMovies([
-							...movies,
-							{
-								movie_id: 4,
-								title: "Harry Potter and the Goblet of Fire",
-								release_year: 2005,
-							},
-						]);
-					}}
-				>
-					More
-				</button>
-			)}
-		</>
-	);
+  const { movies, setMovies } = useContext(MovieContext);
+  return (
+    <>
+      <h1>MoviesList</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.movie_id}>
+            {movie.title} ({movie.release_year})
+          </li>
+        ))}
+      </ul>
+      {movies.length < 4 && (
+        <button
+          onClick={() => {
+            setMovies([
+              ...movies,
+              {
+                movie_id: 4,
+                title: "Harry Potter and the Goblet of Fire",
+                release_year: 2005,
+              },
+            ]);
+          }}
+        >
+          More
+        </button>
+      )}
+    </>
+  );
 }
 export default MovieList;
+
